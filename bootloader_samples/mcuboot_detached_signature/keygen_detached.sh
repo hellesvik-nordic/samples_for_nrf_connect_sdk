992925df858dc10ed0ad2bfeb746dd48703c18e9
Hellesvik <sihe@sihe-desktop.nordicsemi.no>
2022-03-30 08:33:26 +0200

    Make both public key formats default to fix detached key script.

--- bootloader_samples/mcuboot_detached_signature/keygen_detached.sh
+++ bootloader_samples/mcuboot_detached_signature/keygen_detached.sh
@@ -12,7 +12,7 @@ openssl rsa -in keys/priv.pem -pubout -out keys/pub.pem -outform PEM -RSAPublicK
 imgtool2.py sign -v 1.0.0 -H 0x200 -S 0x79e00 --align 8  --fix-sig-pubkey keys/pub.pem --vector-to-sign payload  --pad-header build/zephyr/zephyr.hex keys/data_to_sign.bin
 openssl dgst -sha256  -sigopt rsa_padding_mode:pss -sigopt rsa_pss_saltlen:32 -sigopt rsa_mgf1_md:sha256 -sign keys/priv.pem -out keys/openssl_sig.sig keys/data_to_sign.bin
 openssl base64 -in keys/openssl_sig.sig -out keys/openssl_sig_base64.sig
-imgtool2.py sign -v 1.0.0  -H 0x200 -S 0x79e00 --align 8 --fix-sig keys/openssl_sig_base64.sig --fix-sig-pubkey keys/pub.pem --public-key-format full --pad-header  build/zephyr/zephyr.hex signed_app_image.hex
+imgtool2.py sign -v 1.0.0  -H 0x200 -S 0x79e00 --align 8 --fix-sig keys/openssl_sig_base64.sig --fix-sig-pubkey keys/pub.pem --pad-header  build/zephyr/zephyr.hex signed_app_image.hex
 sleep 2
 nrfjprog --sectorerase --program signed_app_image.hex --reset
 
