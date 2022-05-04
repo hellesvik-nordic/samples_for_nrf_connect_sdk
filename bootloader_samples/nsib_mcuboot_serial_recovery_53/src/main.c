95674210440ac85e6aa95b752c905f0eb8f5089c
Hellesvik <sihe@sihe-desktop.nordicsemi.no>
2022-05-04 14:34:03 +0200

    Removed pm.yml made it work. I still have not been able to make it all work yet though.

--- bootloader_samples/nsib_mcuboot_serial_recovery_53/src/main.c
+++ bootloader_samples/nsib_mcuboot_serial_recovery_53/src/main.c
@@ -11,7 +11,7 @@
 void main(void)
 {
     img_mgmt_register_group();
-	printk("BBB  Hello World! %s\n", CONFIG_BOARD);
+	printk("CCC  Hello World! %s\n", CONFIG_BOARD);
     while(1){
         k_sleep(K_SECONDS(1));
     }
