df4a7cea1c6dbab11198ca3de3995b81c39f6805
Hellesvik <sihe@sihe-desktop.nordicsemi.no>
2022-05-20 15:26:16 +0200

    Removed mcumgr funcionality, as this sample uses serial recovery

--- bootloader_samples/mcuboot_serial_recovery_53/src/main.c
+++ bootloader_samples/mcuboot_serial_recovery_53/src/main.c
@@ -6,12 +6,10 @@
 
 #include <zephyr.h>
 #include <sys/printk.h>
-#include "img_mgmt/img_mgmt.h"
 
 void main(void)
 {
-    img_mgmt_register_group();
-	printk("BBB  Hello World! %s\n", CONFIG_BOARD);
+	printk("Hello World! %s\n", CONFIG_BOARD);
     while(1){
         k_sleep(K_SECONDS(1));
     }
