1ac65fa72ceeb47b255b4ffe073f58c9fe55f4e5
Hellesvik <sihe@sihe-desktop.nordicsemi.no>
2023-07-06 13:08:03 +0200

    2.3.0 support for simultaneous upgrade

--- bootloader_samples/nrf5340/mcuboot_smp_ble_simultaneous/src/main.c
+++ bootloader_samples/nrf5340/mcuboot_smp_ble_simultaneous/src/main.c
@@ -5,12 +5,41 @@
  */
 
 #include <zephyr/kernel.h>
-#include "img_mgmt/img_mgmt.h"
-#include "bluetooth_smp.h"
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+
+#define DEVICE_NAME             CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN         (sizeof(DEVICE_NAME) - 1)
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
 
 void main(void)
 {
-    img_mgmt_register_group();
-    start_smp_bluetooth();
+  int err = 0;
 	printk("AAA this to see it change.\n");
+
+	err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad),
+			      NULL, 0);
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Advertising successfully started\n");
+
 }
