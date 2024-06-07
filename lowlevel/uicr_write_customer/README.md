e37a822f3d30acb82b47f2a7be7e0bfa07ef2734
_urd <sigurd.hellesvik@protonmail.com>
2024-06-07 13:13:45 +0200

    Added CUSTOMER sample

---
+++ lowlevel/uicr_write_customer/README.md
@@ -0,0 +1,6 @@
+## CUSTOMER
+Here is an example on how to write to the CUSTOMER register in UICR
+Tested for the nRF52840.
+
+
+Newer chips have The OTP register instead of this.
---
+++ lowlevel/uicr_write_customer/src/main.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <nrfx_nvmc.h>
+
+#define CUSTOMER0_TEST_DATA ((uint32_t)0x12345678)
+
+int main(void)
+{
+	printk("CUSTOMER0 before: %x\n",(uint32_t)NRF_UICR->CUSTOMER[0]);
+	bool is_writable = nrfx_nvmc_word_writable_check((uint32_t)&NRF_UICR->CUSTOMER[0],CUSTOMER0_TEST_DATA);
+
+	if(is_writable){
+		nrfx_nvmc_word_write((uint32_t)&NRF_UICR->CUSTOMER[0],CUSTOMER0_TEST_DATA);
+		printk("Written\n");
+	} else {
+		printk("Not written\n");
+	}
+
+	printk("CUSTOMER0 after: %x\n",(uint32_t)NRF_UICR->CUSTOMER[0]);
+
+
+
+	return 0;
+}
